bdd83d27713f23c81055816abc3046d209b4f082
marcoviappiani <marco.viappiani@gmail.com>
2015-07-25 11:50:15 -0700

    Passed both tests - Chatterbox client not working properly

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,7 @@ $(function() {
   app = {
 //TODO: The current 'addFriend' function just adds the class 'friend'
 //to all messages sent by the user
-    server: 'https://api.parse.com/1/classes/chatterbox/',
+    server: 'http://127.0.0.1:3000/classes/messages',
     username: 'anonymous',
     roomname: 'lobby',
     lastMessageId: 0,
@@ -63,6 +63,7 @@ $(function() {
         data: { order: '-createdAt'},
         success: function(data) {
           console.log('chatterbox: Messages fetched');
+          app.stopSpinner();
 
           // Don't bother if we have nothing to work with
           if (!data.results || !data.results.length) { return; }
@@ -70,7 +71,6 @@ $(function() {
           // Get the last message
           var mostRecentMessage = data.results[data.results.length-1];
           var displayedRoom = $('.chat span').first().data('roomname');
-          app.stopSpinner();
           // Only bother updating the DOM if we have a new message
           if (mostRecentMessage.objectId !== app.lastMessageId || app.roomname !== displayedRoom) {
             // Update the UI with the fetched rooms
